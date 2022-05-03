442203040bba30a793fae4049e9dd9bcd1ebee24
davquipe <davidcondori99@gmail.com>
2022-05-03 14:13:37 -0500

    Configure sockets en la clase Server

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 const Server = require("./models/server")
 
-
 const server = new Server()
 
 server.execute()
-
-
-
-// const app = require('express')();
-
-// const io = require('socket.io')(server);
-
-
-
-// io.on('connection', ( socket ) => {
-    
-//     // socket.emit('mensaje-bienvinida', {
-//     //     msg: 'Mensaje del server',
-//     //     fecha: new Date() 
-//     // })
-
-//     socket.on('mensaje-to-server', (data) => {
-//         console.log(data);
-
-//         io.emit('mensaje-from-server', data);
-//     })
-    
-// });
-
